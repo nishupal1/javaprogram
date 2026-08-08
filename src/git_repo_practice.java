56eaeb7b1f4ff35d9fe2862ea6b948b443632de9
Nishu Pal <pal447479@gmail.com>
2026-08-08 20:42:59 +0530

    Updated

---
+++ src/git_repo_practice.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class git_repo_practice {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Whats your name ?");
+        String a = sc.next();
+
+
+
+
+    }
+}
