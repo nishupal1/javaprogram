d8ef7232ef9bd470bec8a0faab4db3d6246bf99f
Nishu Pal <pal447479@gmail.com>
2026-08-14 21:33:48 +0530

    All Updated

--- src/git_repo_practice.java
+++ src/git_repo_practice.java
@@ -18,6 +18,8 @@ public class git_repo_practice {
         System.out.println("A another update for github");
         System.out.println("A another update for github");
         System.out.println("Multiple comment for git hub");
+        System.out.println("stucked");
+
 
     }
 }
