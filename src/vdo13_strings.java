7215a1ac6b868870791d7f19c1868c05b77900a1
Nishu Pal <pal447479@gmail.com>
2026-08-16 21:16:29 +0530

    Sting Chapter

---
+++ src/vdo13_strings.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class vdo13_strings {
+    public static void main(String[] args) {
+        String name = new String("Nishu pal");
+        System.out.println(name);
+        String name1 = "Nishu Pal";
+        System.out.println(name1);
+
+
+
+
+    }
+
+}
