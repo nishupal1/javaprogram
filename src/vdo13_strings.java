0bc3116cbbcb8c67e29d1c27662be7ba530747d3
Nishu Pal <pal447479@gmail.com>
2026-08-17 11:36:25 +0530

    Sting Chapter

--- src/vdo13_strings.java
+++ src/vdo13_strings.java
@@ -1,4 +1,6 @@
 package com.company;
+import java.util.Scanner;
+
 
 public class vdo13_strings {
     public static void main(String[] args) {
@@ -6,6 +8,20 @@ public class vdo13_strings {
         System.out.println(name);
         String name1 = "Nishu Pal";
         System.out.println(name1);
+        int a = 12;
+        float b = 2.23f;
+        System.out.printf("The value of a is %d and value of b is %f%n", a, b);
+        System.out.format("The value of a is %d and the value of b is %100.2f\n", a, b);
+        //%d for int, %f for float, 100 means 100 space, .2 means two decimal,
+        // \n means break line, fromat and print is same, sc.next for one word, nextline for whool line
+
+        Scanner sc = new Scanner(System.in);
+        String st = sc.next();
+        System.out.print(st);
+        String st1 = sc.nextLine();
+        System.out.println(st1);
+
+
 
 
 
