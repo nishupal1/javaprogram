a9d559fd3fb50885a10537b9089752f3e359ad41
Nishu Pal <pal447479@gmail.com>
2026-08-23 21:13:26 +0530

    if else condition

---
+++ src/vdo16_conditions.java
@@ -0,0 +1,10 @@
+public class vdo16_conditions {
+    static void main(String[] args) {
+        int age = 19;
+        if(age>18){
+            System.out.println("Yes, You can drive now!");
+        }
+        else {
+            System.out.println("No, You cannot drive yet!");}
+    }
+}
