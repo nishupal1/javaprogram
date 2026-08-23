7226982af1750deb6ba7ac992fc2f6df4b08ee9f
Nishu Pal <pal447479@gmail.com>
2026-08-23 21:37:50 +0530

    value assign & equality

--- src/vdo16_conditions.java
+++ src/vdo16_conditions.java
@@ -1,10 +1,18 @@
 public class vdo16_conditions {
     static void main(String[] args) {
-        int age = 16;
-        if(age>18){
+        int age = 18;  // here 18 is assigne value
+        if(age >= 18){
             System.out.println("Yes, You can drive now!");
         }
         else {
             System.out.println("No, You cannot drive yet!");}
+
+        int age1 = 15;
+        if(age1 == 18) {  // here == is to check equality
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
     }
 }
